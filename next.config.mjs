37ca148f747f222533630b4f82c9af1057b6e630
Luisferrer02 <luisferrer2002@gmail.com>
2025-09-18 15:13:13 +0200

    portfolio path

--- next.config.mjs
+++ next.config.mjs
@@ -3,16 +3,16 @@ const isProd = process.env.NODE_ENV === 'production'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',                                // genera /out con `next build`
-  basePath: isProd ? '/portfolio' : '',    // ⚠️ pon aquí el nombre EXACTO de tu repo
+  output: 'export',                 // genera /out con `next build`
+  basePath: isProd ? '/portfolio' : '',
   images: { unoptimized: true },
 
-  // Exponer basePath al cliente para construir rutas a assets (icons, pdf, etc.)
+  // Exponer basePath al cliente para construir rutas a assets (icons/pdf)
   env: {
-    NEXT_PUBLIC_BASE_PATH: isProd ? 'portfolio' : ''
+    NEXT_PUBLIC_BASE_PATH: isProd ? '/portfolio' : ''
   },
 
-  // Útil para servir assets estáticos bajo basePath
+  // Prefijo para assets estáticos (útil en Pages)
   assetPrefix: isProd ? '/portfolio/' : ''
 }
 
