b2b65ba5a94e84d5b09dae70aebd38c1f8752410
Luisferrer02 <luisferrer2002@gmail.com>
2025-09-19 18:48:32 +0200

    namesicon

--- pages/index.js
+++ pages/index.js
@@ -221,14 +221,14 @@ export default function Home() {
               {[
                 { label: "JavaScript", icon: "javascript.svg" },
                 { label: "React", icon: "react.svg" },
+                { label: "Node.js", icon: "node.svg" },
                 {
                   label: "Next.js",
-                  iconLight: "nextjs-light.png",
-                  iconDark: "nextjs-dark.png",
+                  iconLight: "nextjs.png",
+                  iconDark: "nextjslight.webp",
                 },
-                { label: "Node.js", icon: "node.svg" },
                 { label: "Java", icon: "java.svg" },
-                { label: "HTML/CSS", icon: "htmlcss.svg" },
+                { label: "HTML/CSS", icon: "html.svg" },
                 { label: "Kotlin", icon: "kotlin.svg" },
                 { label: "C++", icon: "cpp.svg" },
               ].map((s) => (
@@ -242,9 +242,10 @@ export default function Home() {
             </h3>
             <div className="flex flex-wrap gap-2">
               {[
-                { label: "SQL (Advanced)", icon: "sql.svg" },
-                { label: "MongoDB (Advanced)", icon: "mongodb.svg" },
-                { label: "Redis (Proficient)", icon: "redis.svg" },
+                { label: "SQL", icon: "sql.svg" },
+                { label: "MongoDB", icon: "mongodb.svg" },
+                { label: "Redis", icon: "redis.svg" },
+                { label: "Neo4j", icon: "neo4j.png" },
               ].map((s) => (
                 <SkillBadge key={s.label} {...s} />
               ))}
@@ -256,11 +257,11 @@ export default function Home() {
             </h3>
             <div className="flex flex-wrap gap-2">
               {[
-                { label: "Git", icon: "git.svg" },
-                { label: "Agile/Scrum", icon: "agile.svg" },
+                { label: "Git", icon: "github.svg" },
+                { label: "Agile", icon: "jira.svg" },
                 { label: "Linux", icon: "linux.svg" },
                 { label: "Docker", icon: "docker.svg" },
-                { label: "Jupyter Notebooks", icon: "jupyter.svg" },
+                { label: "Jupyter", icon: "jupyter.svg" },
               ].map((s) => (
                 <SkillBadge key={s.label} {...s} />
               ))}
@@ -270,10 +271,9 @@ export default function Home() {
             <h3 className="font-extrabold mb-3">{t("skills.groups.dsai")}</h3>
             <div className="flex flex-wrap gap-2">
               {[
-                { label: "Python (Advanced)", icon: "python.svg" },
-                { label: "Deep Learning", icon: "dl.svg" },
+                { label: "Python", icon: "python.svg" },
                 { label: "Machine Learning", icon: "ml.svg" },
-                { label: "CNN", icon: "cnn.svg" },
+                { label: "CNN", icon: "cnn.png" },
               ].map((s) => (
                 <SkillBadge key={s.label} {...s} />
               ))}
